de2e4e7c748be81f0f873b891b1775f2261d183b
abdullah <144612458+abdullah71006517@users.noreply.github.com>
2024-11-25 18:26:30 +0600

    Add files via upload

---
+++ syntex of class and object decleration and valu assin.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+/// decalr a class
+class Object{
+// acess modifire
+public :
+    // taken tow attrubutes
+    string name ;
+    int age ;
+};
+
+int main(){
+Object  object_varable ;// main  fucation ojbect varibale
+// value assing
+object_varable.age=10;//
+cout<<object_varable.age<<endl;
+object_varable.name="abdullah";
+cout<<object_varable.name;
+return 0;
+}
