7e120dd44fb75d6819e64ac7532dff20f2a00469
abdullah <144612458+abdullah71006517@users.noreply.github.com>
2024-11-27 18:52:21 +0600

    Update pass by class pass to fucation.cpp

--- pass by class pass to fucation.cpp
+++ pass by class pass to fucation.cpp
@@ -8,10 +8,12 @@ public :
     string name ;
     int age ;
 };
+// /call by value
 void input(Object      object_varable){
 
 cin>>object_varable.age;
 cin>>object_varable.name;
+/// i will print only inside fuction
 cout<<object_varable.age<<endl;
 cout<<object_varable.name<<endl;
 
@@ -22,18 +24,18 @@ cout<<object_varable.name<<endl;
 void Print(Object      object_varable){
 cout<<object_varable.age<<endl;
 cout<<object_varable.name<<endl;
-object_varable.age=20;// modify
+
 
 return ;
 
 }
 int main(){
 Object  object_varable ;
-//object_varable.age=10;
-//object_varable.name="abdullah";
+object_varable.age=10;
+object_varable.name="abdullah";
 input(object_varable );
   Print(object_varable);///call by value
-  //cout<<object_varable.age<<endl;/// outsite
+
 return 0;
 }
 
